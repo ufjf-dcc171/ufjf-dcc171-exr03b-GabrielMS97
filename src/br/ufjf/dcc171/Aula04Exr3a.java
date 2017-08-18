655621bf253991909fb41fae9231ac27077d0973
ice <ice@localhost>
2017-08-17 22:56:55 -0300

    Exercício 4 - Falta Calcular as raízes

---
+++ src/br/ufjf/dcc171/Aula04Exr3a.java
@@ -0,0 +1,15 @@
+package br.ufjf.dcc171;
+
+import javax.swing.JFrame;
+
+public class Aula04Exr3a {
+
+    public static void main(String[] args) {
+        Janela janela = new Janela();
+        janela.setSize(200, 300);
+        janela.setVisible(true);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janela.setLocationRelativeTo(null);
+    }
+
+}
